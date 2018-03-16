61e5ad3f3710efe3789c06d9f81ee931243735ca
vishaljasrotia <vishal.singh8989@gmail.com>
2018-03-16 16:59:03 -0400

    star cubing algorithms.

---
+++ include/starcube.h
@@ -0,0 +1,24 @@
+#ifndef _STAR_CUBE_H
+#define _STAR_CUBE_H
+
+
+#include <map>
+#include <vector>
+#include <string>
+
+#include <startree.h>
+
+
+class StarCube
+{
+
+
+
+public:
+	void starcubing(TreeNode *root, TreeNode* cnode);
+
+
+
+};
+
+#endif
---
+++ src/starcube.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <csv_reader.h>
+#include <logger.h>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <startable.h>
+#include <startree.h>
+#include <starcube.h>
+#include <iomanip>
+
+using namespace std;
+
+
+void StarCube :: starcubing(TreeNode *root, TreeNode* cnode){
+
+
+
+
+
+}
